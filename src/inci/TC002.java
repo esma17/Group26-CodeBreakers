6cbad1ab4aacac7ef5dc2ebedeea7808c01adaa6
incisuinci <incisuinci@gmail.com>
2020-11-12 13:00:34 -0500

    50 years old

--- src/inci/TC002.java
+++ src/inci/TC002.java
@@ -3,5 +3,6 @@ package inci;
 public class TC002 {
     public static void main(String[] args) {
         System.out.println("I like Java !!!");
+        System.out.println("I am 50 years old trying to keep up with youngsters");
     }
 }
